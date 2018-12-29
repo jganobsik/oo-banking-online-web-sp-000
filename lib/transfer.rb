2a2c251bead7eaaac72308e11929a3066c686152
Joe Ganobsik <jganobsik@gmail.com>
2018-12-29 20:15:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   end
   
   def execute_transaction
-      if self.valid? == true && @status == 'pending'
+      if self.valid? == true && @status == 'pending' && @sender.balance > @amount
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = 'complete'
