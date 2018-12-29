9cf842117133ba7bc78f910abf3c0f3c6b7f20ea
Joe Ganobsik <jganobsik@gmail.com>
2018-12-29 20:02:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,9 +23,11 @@ class Transfer
       @receiver.balance += @amount
       @status = 'complete'
     else 
-      return 'Transaction rejected'
+      return 'Transaction rejected. Please check your account balance.'
     end
   end
+  
+  def reverse_transfer
       
   
 end
