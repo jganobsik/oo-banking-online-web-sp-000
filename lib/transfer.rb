e02cc502c838b89e84cf2d2e8ef325a6930f964d
Joe Ganobsik <jganobsik@gmail.com>
2018-12-29 20:16:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -7,7 +7,9 @@ class Transfer
     @status = 'pending'
     if sender.valid? ==  true 
     @sender = sender
-    
+  else 
+    @status = 'rejected'
+  end 
     @receiver = receiver
     end
     
